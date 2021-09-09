b1064201fb6f9c11326d0e8f6c3b42d65ba52431
dewana_kl <dewanakretarta29@gmail.com>
2021-09-09 19:00:44 +0700

    file tugas

--- dragon.java
+++ dragon.java
@@ -1,51 +1,27 @@
+import java.util.Arrays;
+
 public class dragon {
     public static void main(String[] args) {
-        int gold = 0;
-        int d = 0;
-        int k = 0;
-
         int dragon[] = { 7, 2 };
-        int knight[] = { 4, 3, 1, 2 };
-        // int dragon[] = { 5, 4 };
-        // int knight[] = { 7, 8, 4 };
+        int knight[] = { 2, 1, 8, 5 };
 
-        int n = dragon.length;
-        int m = knight.length;
+        Arrays.sort(dragon);
+        Arrays.sort(knight);
 
-        while ((d < n) && (k < m)) {
-            // while {
-            if (knight[k] >= dragon[d]) {
-                gold += knight[k];
-                k++;
+        int total = 0;
+        int d = 0;
+
+        for (int k : knight) {
+            if ((d < dragon.length) && (k >= dragon[d])) {
+                total += k;
                 d++;
-                break;
             }
-            // }
-        }
-        if (d == n) {
-            System.out.println(gold);
-        } else {
-            System.out.println("knight fail");
         }
 
-        // // dragon
-        // for (int i = 0; i < n; i++) {
-        // // knight
-        // for (int j = 0; j < m; j++) {
-        // if (knight[j] >= dragon[i]) {
-        // // gold += knight[j];
-        // System.out.println("Loowater is doomed!" + knight[j]);
-        // i++;
-        // }
-        // // k = m;
-        // }
-        // // d = i;
-        // // if (d == n) {
-        // // System.out.println(gold);
-        // // break;
-        // // } else {
-        // // System.out.println("Loowater is doomed!");
-        // // }
-        // }
+        if (d == dragon.length) {
+            System.out.println(total);
+        } else {
+            System.out.println("Knight Fall");
+        }
     }
 }
